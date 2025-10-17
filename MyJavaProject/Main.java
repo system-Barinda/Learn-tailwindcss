a0e6830a1bcae32778b9030191a7154d757b55f5
system sylvere BARINDA <systembarinda@gmail.com>
2025-10-17 21:27:54 +0200

    done to update the stuffs of java in vs codes

---
+++ MyJavaProject/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 100;
+        int num1 = 200;
+        int sum = num + num1;
+        System.out.println(sum);
+
+    }
+}
